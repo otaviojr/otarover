93be6a990bc9c201721b5f970106bf650265da47
Otavio Ribeiro <otavio.ribeiro@gmail.com>
2017-12-24 23:55:21 +0000

    platform driver

---
+++ driver/platform/blue/blue_drv.h
@@ -0,0 +1,10 @@
+/**
+ * @file   blue_drv.h
+ * @author Otavio Ribeiro
+ * @date   24 Dec 2017
+ * @brief  A kernel module for controlling beaglebone blue board
+ *
+ * TODO: Add license text/version
+ *
+ */
+
